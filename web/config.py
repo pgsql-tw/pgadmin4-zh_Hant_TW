7462bee6ba4c48a337ab38f600eb7d1e887dd178
Yung-Chung Ku <ycku@csie.org>
2018-03-20 16:11:40 +0800

    Add files via upload

---
+++ web/config.py
@@ -0,0 +1,13 @@
+# 請自行更新你的config.py，切勿直接覆蓋！
+
+# Languages we support in the UI
+LANGUAGES = {
+    'en': 'English',
+    'zh': 'Chinese (Simplified)',
+    'de': 'German',
+    'ja': 'Japanese',
+    'pl': 'Polish',
+    'ru': 'Russian',
+    'zh_Hant_TW': 'Mandarin (zh-tw)'
+}
+
